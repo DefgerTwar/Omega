55f3edcfaecc81a3f0e6c398c5c4bf76302b3fe2
VitorGouveia <vitorneves.gouveia10@gmail.com>
2023-04-01 14:05:39 -0300

    add lgpd banner

---
+++ src/components/Policy.tsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Dispatch, SetStateAction } from "react";
+
+export const PolicyBanner = ({
+  accept,
+  setAccept,
+}: {
+  accept: boolean;
+  setAccept: Dispatch<SetStateAction<boolean>>;
+}) => {
+  const router = useRouter();
+
+  const loadScripts = () => {
+    // save in the localStorage the preference
+    localStorage.setItem("privacySettings", "true");
+
+    router.reload();
+
+    setAccept(true);
+  };
+
+  return accept ? null : (
+    <div className="fixed bottom-[70px] flex w-full flex-col items-center gap-3 bg-gray-700 p-6 md:bottom-0">
+      <p className="text-center text-base text-white lg:text-xl">
+        Uso de cookies - Guardamos estatísticas de visitas para melhorar sua
+        experiência de navegação.
+      </p>
+      <p className="text-center text-base text-white lg:text-xl">
+        Ao pressionar “ACEITAR”, você concorda com nossa{" "}
+        <Link href="/privacy">Política de Privacidade</Link>.
+      </p>
+
+      <button
+        className="w-max rounded bg-[#00E560] px-3 py-2 text-white transition-colors hover:bg-[#248F50]"
+        onClick={loadScripts}
+      >
+        ACEITAR
+      </button>
+    </div>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,27 @@ import Script from "next/script";
 import "../globals.css";
 import { Header } from "../components/Header";
 import { BottomBar } from "../components/BottomBar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Footer } from "../components/Footer";
+import { PolicyBanner } from "../components/Policy";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [theme, setTheme] = useState<"light" | "dark">("dark");
+  const [accept, setAccept] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    const setting = localStorage.getItem("privacySettings");
+
+    if (setting) {
+      // if key exists user has accepted
+      setAccept(true);
+    }
+
+    setLoading(false);
+  }, []);
 
   return (
     <>
@@ -22,21 +38,25 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/icon.svg" />
       </Head>
 
-      <Script id="google-tag-manager-head" strategy="afterInteractive">
-        {`
+      {accept && (
+        <Script id="google-tag-manager-head" strategy="afterInteractive">
+          {`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-N26WCHT');
         `}
-      </Script>
+        </Script>
+      )}
 
       <Header theme={theme} setTheme={setTheme} />
 
       <Component {...pageProps} theme={theme} />
 
       <BottomBar theme={theme} />
+
+      {!loading && <PolicyBanner accept={accept} setAccept={setAccept} />}
 
       <Footer theme={theme} />
     </>
