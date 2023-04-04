a522047f3ee0236cb8f0b344239fc9388646dd74
VitorGouveia <vitorneves.gouveia10@gmail.com>
2023-04-04 18:13:36 -0300

    add analytics (dumb way)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -38,16 +38,34 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/icon.svg" />
       </Head>
 
-      {accept ?(
-        <Script id="google-tag-manager-head" strategy="afterInteractive">
-          {`
+      {accept ? (
+        <>
+          <Script id="google-tag-manager-head" strategy="afterInteractive">
+            {`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-N26WCHT');
         `}
-        </Script>
+          </Script>
+
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-88QD0GKD93%22%3E"
+            strategy="afterInteractive"
+          />
+
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-88QD0GKD93');
+            `}
+          </Script>
+        </>
       ) : null}
 
       <Header theme={theme} setTheme={setTheme} />
