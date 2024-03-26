6d1b10ebffff371970268a105cea2fde9e4e25bc
VitorGouveia <vitorneves.gouveia10@gmail.com>
2024-03-26 12:50:34 -0300

    add LPIs, implement desktop layout

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config"
+
+import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+    }),
+  ],
+})
